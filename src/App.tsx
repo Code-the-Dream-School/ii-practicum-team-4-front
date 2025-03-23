642bb6c2770f470181a10f91e7f4575ca0034447
Katya Suhr <38622937+katy1313@users.noreply.github.com>
2025-03-23 11:28:20 -0400

    docs/tsconfig (#2)
    * docs/tsconfig
    
    * docs/tsconfig fixes
    
    * docs/fixes
    
    * docs/tsconfig changes

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { getAllData } from './util/index';
+
+const URL = 'http://localhost:8000/api/v1/';
+
+const App = () => (
+  <>
+      <h1>hello</h1>
+  </>
+);
+
+export default App;
