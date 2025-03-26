49fe8d9f43fed0c006843c6975062d88b3b3aa4d
Anna Bobrova <spomandarin@gmail.com>
2025-03-25 21:41:23 -0500

    chore: update configure tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
-// import react from '@vitejs/plugin-react'
-import vue from "@vitejs/plugin-vue";
+import react from "@vitejs/plugin-react";
+// import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
