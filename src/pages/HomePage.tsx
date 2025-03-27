848666416fdd707096b418851b76885ebe169b9e
Anna Bobrova <spomandarin@gmail.com>
2025-03-27 13:50:31 -0500

    chore: new update configure tailwind

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <div className="home">
-      <h1 class="text-3xl font-bold underline">Home Page</h1>
+      <h1 className="text-3xl font-bold underline">Home Page</h1>
       <Link to="/shop">Shop </Link>
       <Link to="/about">About</Link>
       <Link to="/account">Account </Link>
