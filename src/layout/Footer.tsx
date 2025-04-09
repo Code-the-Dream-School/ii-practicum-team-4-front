004dab793660ed618579de1527ca6c64a4a619f4
Boris Khenzykhenov <123456789+odin@users.noreply.github.com>
2025-04-09 17:59:32 -0400

    feat: color for footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,18 +1,18 @@
 import React from "react";
+import '.../src/index.css';
 
 const currentYear = new Date().getFullYear();
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-green/700 text-primary py-8">
+    <footer className="bg-primary text-background py-6 px-4 mt-8">
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
-       
         <div className="flex items-center space-x-3">
             <img
                 src={"../assets/images/Logo.svg"}
                 alt="Farm2You Logo"
-                className="w-12 h-12"
+                className="w-10 h-10"
             />
-          <span className="text-2xl font-semibold">
+          <span className="text-l font-semibold">
             <ul>
                 <li>Mon-Fri: 9 AM - 6 PM</li>
                 <li>Sat: 10 AM - 4 PM</li>
@@ -20,7 +20,30 @@ const Footer: React.FC = () => {
             </ul>
           </span>
         </div>
-
+        <div className=''>
+            <h3 className=''>Quick Links</h3>
+            <ul className=''>
+                <li>
+                    <a href="/about" className=''>About Us</a>
+                </li>
+                <li>
+                    <a href="/account" className=''>Account</a>
+                </li>
+                <li>
+                    <a href="/checkout" className=''>Checkout</a>
+                </li>
+                <li>
+                    <a href="/shop" className=''>Shop Now</a>
+                </li>
+            </ul>
+        </div>
+        <span className="text-l font-semibold">
+            <ul>
+                <li>example@gmail.com</li>
+                <li>000-000-0000</li>
+                <li>000-000-0000</li>
+            </ul>
+          </span>
         <div className="flex space-x-6 mt-4 md:mt-0">
           <a
             href="https://facebook.com"
