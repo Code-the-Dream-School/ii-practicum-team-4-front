6bf506a26a27e87e44cdfe6d7c75a6139f6dfb17
Katya Suhr <38622937+katy1313@users.noreply.github.com>
2025-05-06 11:47:28 -0400

    feat:added cart_context to cart page (#28)
    * feat:added cart_context to cart page
    
    * dynamic cart update
    
    * cart is empty added

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,82 +1,108 @@
-import { useState } from 'react';
 import Button from '../components/Button';
-import lettuce from '../assets/images/cabbage.png';
 import trash_can from '../assets/images/icons/trash_can.svg';
+import { useCart } from '../contexts/CartContext';
+import { Link } from 'react-router-dom';
 
 const CartPage = () => {
-  const products = Array.from({ length: 15 }, (_, i) => ({
-    id: i,
-    name: 'Chinese cabbage',
-    weight: '1 pound',
-    image: lettuce,
-  }));
-
-  const [quantity, setQuantity] = useState<{ [key: number]: number }>({});
-
-  const handleIncrement = (productId: number) => {
-    setQuantity((prev) => ({
-      ...prev,
-      [productId]: (prev[productId] || 0) + 1,
-    }));
+  const { cart, addToCart, removeFromCart, total, boxSize } = useCart();
+  const getProductQuantity = (id: number) => {
+    return cart.find((item) => item.id === id)?.quantity || 0;
   };
 
-  const handleDecrement = (productId: number) => {
-    setQuantity((prev) => ({
-      ...prev,
-      [productId]: Math.max((prev[productId] || 0) - 1, 0),
-    }));
+  const increaseProductQuantity = (productId: number) => {
+    const product = cart.find((p) => p.id === productId);
+    if (product) {
+      addToCart(
+        {
+          id: product.id,
+          name: product.name,
+          image: product.image,
+        },
+        1
+      );
+    }
+  };
+
+  const decreaseProductQuantity = (productId: number) => {
+    const currentQty = getProductQuantity(productId);
+    if (currentQty > 0) {
+      const product = cart.find((p) => p.id === productId);
+      if (product) {
+        addToCart(
+          {
+            id: product.id,
+            name: product.name,
+            image: product.image,
+          },
+          -1
+        );
+      }
+    }
+  };
+
+  const handleRemoveItem = (productId: number) => {
+    removeFromCart(productId);
   };
 
   return (
     <div className="md:m-20 md:flex md:flex-row md:justify-between lg:mx-20 lg:justify-center">
-      <ul className="border-yellow bg-form-light mx-auto mb-14 flex w-full max-w-xs flex-col self-start rounded-lg border md:mx-auto md:w-3/5 md:max-w-md lg:mx-10 lg:w-2/3 lg:max-w-full">
-        <li className="text-secondary m-4 font-semibold md:mr-20">
-          <span className="float-left">Product</span>
-          <span className="float-right pr-8">Quantity</span>
-        </li>
-        <hr className="border-yellow"></hr>
-        {products.map((product) => (
-          <>
-            <div key={product.id}>
-              <li className="bg-form-light m-2 flex flex-row justify-between border-none p-2 text-center md:justify-start">
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="h-10 w-auto object-contain md:h-20"
-                />
-                <p className="font-subtext text-secondary mx-2 max-w-28 overflow-hidden align-middle text-sm overflow-ellipsis whitespace-nowrap md:mt-2 md:overflow-visible md:text-left md:text-lg">
-                  {product.name}
-                  <br />
-                  <span className="text-primary">{product.weight}</span>
-                </p>
-                <div className="text-secondary ml-auto flex flex-row items-center gap-2 md:gap-4">
-                  <button
-                    className="h-8 w-8 items-center justify-center rounded-full bg-white text-black hover:opacity-80"
-                    onClick={() => handleDecrement(product.id)}
-                  >
-                    -
-                  </button>
-                  <span>{quantity[product.id] || 0}</span>
-                  <button
-                    className="bg-error h-8 w-8 items-center justify-center rounded-full text-white hover:opacity-80"
-                    onClick={() => handleIncrement(product.id)}
-                  >
-                    +
-                  </button>
-                  <button>
+      {cart.length === 0 ? (
+        <p className="text-secondary text-2xl mr-auto font-semibold">Your cart is empty</p>
+      ) : (
+        <>
+          <ul className="border-yellow bg-form-light mx-auto mb-14 flex w-full max-w-xs flex-col self-start rounded-lg border md:mx-auto md:w-3/5 md:max-w-md lg:mx-10 lg:w-2/3 lg:max-w-full">
+            <li className="text-secondary m-4 font-semibold md:mr-20">
+              <span className="float-left">Product</span>
+              <span className="float-right pr-8">Quantity</span>
+            </li>
+            <hr className="border-yellow"></hr>
+            {cart.map((product) => (
+              <>
+                <div key={product.id}>
+                  <li className="bg-form-light m-2 flex flex-row justify-between border-none p-2 text-center md:justify-start">
                     <img
-                      src={trash_can}
-                      alt="Trash Can"
-                      className="ml-4 hidden h-6 w-5 hover:opacity-80 md:block"
+                      src={product.image}
+                      alt={product.name}
+                      className="h-10 w-auto object-contain md:h-20"
                     />
-                  </button>
+                    <p className="font-subtext text-secondary mx-2 max-w-28 overflow-hidden align-middle text-sm overflow-ellipsis whitespace-nowrap md:mt-2 md:overflow-visible md:text-left md:text-lg">
+                      {product.name}
+                      <br />
+                      {/* <span className="text-primary">{product.weight}</span> */}
+                    </p>
+                    <div className="text-secondary ml-auto flex flex-row items-center gap-2 md:gap-4">
+                      <button
+                        className="h-8 w-8 items-center justify-center rounded-full bg-white text-black hover:opacity-80"
+                        onClick={() => decreaseProductQuantity(product.id)}
+                      >
+                        -
+                      </button>
+                      <span className="w-8 text-center">
+                        {getProductQuantity(product.id)}
+                      </span>
+                      <button
+                        className="bg-error h-8 w-8 items-center justify-center rounded-full text-white hover:opacity-80"
+                        onClick={() => increaseProductQuantity(product.id)}
+                      >
+                        +
+                      </button>
+                      <button onClick={() => handleRemoveItem(product.id)}>
+                        <img
+                          src={trash_can}
+                          alt="Trash Can"
+                          className="ml-4 hidden h-6 w-5 hover:opacity-80 md:block"
+                        />
+                      </button>
+                    </div>
+                  </li>
                 </div>
-              </li>
-            </div>
-            <hr className="border-yellow mx-2 hidden last:hidden md:block"></hr>
-          </>
-        ))}
-      </ul>
+                <hr className="border-yellow mx-2 hidden last:hidden md:block"></hr>
+              </>
+            ))}
+          </ul>
+        </>
+      )}
+
       <div className="border-yellow bg-form-light md:max-w-m mx-auto mb-14 w-full max-w-xs self-start rounded-lg border px-4 py-6 lg:w-1/3 lg:max-w-full">
         <h3 className="font-subtext text-primary pb-2 text-xl font-semibold tracking-wide">
           Cart Total
@@ -85,12 +111,14 @@ const CartPage = () => {
           <li className="text-secondary my-4 text-sm">
             Box:{' '}
             <span className="text-primary float-right font-semibold">
-              Large x2, Small x1
+              {boxSize}
             </span>
           </li>
           <li className="text-secondary my-4 text-sm">
             Subtotal:{' '}
-            <span className="text-primary float-right font-bold">$145.00</span>
+            <span className="text-primary float-right font-bold">
+              ${total.toFixed(2)}
+            </span>
           </li>
           <li className="text-secondary my-4 text-sm">
             Shipping:{' '}
@@ -98,11 +126,15 @@ const CartPage = () => {
           </li>
           <li className="text-secondary mb-6 text-sm">
             Total:{' '}
-            <span className="text-primary float-right font-bold">$145.00</span>
+            <span className="text-primary float-right font-bold">
+              ${total.toFixed(2)}
+            </span>
           </li>
         </ul>
         <div className="text-center">
-          <Button text="Proceed To Checkout" type="button" color="primary" />
+          <Link to="/checkout" target="_self">
+            <Button text="Proceed To Checkout" type="button" color="primary" />
+          </Link>
         </div>
       </div>
     </div>
